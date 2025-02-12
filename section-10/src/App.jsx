bceae809ee07112d2262e00c1f7706ac8f04837e
Carina Booij <carinabooij@gmail.com>
2025-02-12 17:35:26 +0100

    component composition

--- section-10/src/App.jsx
+++ section-10/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
+import Product from './components/Product.jsx';
+
 import { DUMMY_PRODUCTS } from './dummy-products.js';
 
 function App() {
@@ -12,7 +14,6 @@ function App() {
   function handleAddItemToCart(id) {
     setShoppingCart((prevShoppingCart) => {
       const updatedItems = [...prevShoppingCart.items];
-      console.log('it works');
 
       const existingCartItemIndex = updatedItems.findIndex(
         (cartItem) => cartItem.id === id
@@ -72,7 +73,13 @@ function App() {
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
     </>
   );
 }
